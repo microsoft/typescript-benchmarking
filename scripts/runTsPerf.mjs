5bbd1f57658aacf04d31271d563c717c79eab73c
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-08-16 13:51:51 -0700

    When the commit hash is the latest on the ref, upload it as latest

--- scripts/runTsPerf.mjs
+++ scripts/runTsPerf.mjs
@@ -56,6 +56,18 @@ async function getRepoInfo() {
 }
 
 /**
+ * @param {string} ref
+ * @param {string} commit
+ */
+async function isLatestCommitForRef(ref, commit) {
+    const auth = getNonEmptyEnv("GH_TOKEN");
+    const { Octokit } = await import("@octokit/rest");
+    const gh = new Octokit({ auth });
+    const latest = await gh.git.getRef({ owner: "microsoft", repo: "typescript", ref });
+    return latest.data.object.sha === commit;
+}
+
+/**
  * @param {string} name
  * @param {(string | undefined)[]} hostVars
  * @returns {string[]}
@@ -134,6 +146,26 @@ async function getCommonBenchmarkArgs(
                 "--save",
                 `blob:${info.branch}/${info.timestampDir}/${info.commitShort}.${args.saveBlob}.benchmark`,
             );
+
+            // If the current commit is the latest for the given ref, then we save it as such.
+            // This is generally safe because even if we are currently the most recent build for a ref
+            // and this returns false, then it must be the case that the newer commit will get enqueued
+            // at some point in the future.
+            //
+            // Note that there is a chance that "latest" will lag behind; if we get lots of merges
+            // in a row and Pipelines happens to run them in order, then each will say "I'm not latest",
+            // and skip the upload, until the actual latest happens to run. But, anecdotally, Pipelines
+            // likes to run builds out of order, often with the _newest_ build first, so this should be fine.
+            // If this turns out not to be fine, we can come up with something fancier, but since we don't
+            // use baselines to test PRs anymore, it's likely not harmful.
+            const ref = getNonEmptyEnv("REF");
+            const latest = await isLatestCommitForRef(ref, info.commit);
+            if (latest) {
+                tsperfArgs.push(
+                    "--save",
+                    `blob:${info.branch}/latest.${args.saveBlob}.benchmark`,
+                );
+            }
         }
 
         if (args.baseline) {
