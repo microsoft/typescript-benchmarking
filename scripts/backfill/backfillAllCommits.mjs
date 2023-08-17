e96307591e6317d089a0ff43db855a261dc8bd84
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-08-16 20:07:45 -0700

    Move backfill script and add one that runs specific branches only

--- scripts/backfill/backfillAllCommits.mjs
+++ scripts/backfill/backfillAllCommits.mjs
@@ -1,12 +1,16 @@
+/**
+ * This script backfills all commits on a branch from a given starting commit.
+ */
+
 import assert from "assert";
-import vsts from "azure-devops-node-api";
 import dotenv from "dotenv";
 import { execa } from "execa";
 import { appendFileSync, readFileSync } from "fs";
 import minimist from "minimist";
 import ora from "ora";
 
-import { sleepSeconds } from "./utils.mjs";
+import { sleepSeconds } from "../utils.mjs";
+import { getPendingBuildCount, runPipeline } from "./common.mjs";
 
 dotenv.config();
 
@@ -33,9 +37,6 @@ assert(branch, "Must provide --branch");
 const pipeline = args.pipeline;
 assert(pipeline, "Must provide --pipeline");
 
-const vstsToken = process.env.VSTS_TOKEN;
-assert(vstsToken, "Must provide VSTS_TOKEN environment variable.");
-
 try {
     await execa("git", ["merge-base", "--is-ancestor", startCommit, branch], { cwd: checkout });
 }
@@ -44,87 +45,6 @@ catch {
     process.exit(1);
 }
 
-const unset = Symbol();
-
-/**
- * @type {<T>(fn: () => T) => () => T}
- */
-function memoize(fn) {
-    /** @type {any} */
-    let v = unset;
-    return () => {
-        if (v !== unset) {
-            return v;
-        }
-        return (v = fn());
-    };
-}
-
-const project = "cf7ac146-d525-443c-b23c-0d58337efebc";
-
-const getBuildApi = memoize(() => {
-    const token = process.env.VSTS_TOKEN;
-    assert(token);
-
-    const vstsTypescript = new vsts.WebApi(
-        "https://typescript.visualstudio.com/defaultcollection",
-        vsts.getPersonalAccessTokenHandler(token),
-    );
-    return vstsTypescript.getBuildApi();
-});
-
-async function getPendingBuildCount() {
-    const build = await getBuildApi();
-    const builds = await build.getBuilds(
-        project,
-        [args.pipeline],
-        undefined,
-        undefined,
-        undefined,
-        undefined,
-        undefined,
-        undefined,
-        /** @type {any} */ (1 | 32), // BuildStatus.InProgress | BuildStatus.NotStarted
-    );
-    return builds.length;
-}
-
-/**
- * @typedef {{
- *     resources?: {
- *         repositories?: Record<string, { refName?: string; version?: string } | undefined>;
- *     };
- *     variables?: Record<string, { isSecret?: boolean; value?: string; } | undefined>;
- *     templateParameters?: Record<string, string | number | boolean | undefined>;
- * }} PipelineRunArgs
- *
- * @typedef {{
- *     _links: { web: { href: string }; };
- * }} PipelineRunResult
- */
-
-/**
- * @param {number} pipelineId
- * @param {PipelineRunArgs} args
- * @returns {Promise<PipelineRunResult>}
- */
-async function runPipeline(pipelineId, args) {
-    const build = await getBuildApi();
-
-    // https://github.com/microsoft/azure-devops-go-api/blob/8dbf8bfd3346f337d914961fab01df812985dcb8/azuredevops/v7/pipelines/client.go#L446
-    const verData = await build.vsoClient.getVersioningData(
-        "7.1-preview.1",
-        "pipelines",
-        "7859261e-d2e9-4a68-b820-a5d84cc5bb3d",
-        { project, pipelineId },
-    );
-    const url = verData.requestUrl;
-    const options = build.createRequestOptions("application/json", verData.apiVersion);
-    assert(url);
-
-    const response = await build.rest.create(url, args, options);
-    return response.result;
-}
 /**
  * @typedef {{
  *     hash: string;
@@ -213,7 +133,7 @@ for (let i = 0;; i++) {
     }
 
     while (true) {
-        const count = await getPendingBuildCount();
+        const count = await getPendingBuildCount(+args.pipeline);
         if (count === 0) {
             break;
         }
---
+++ scripts/backfill/backfillLatestOnBranch.mjs
@@ -0,0 +1,39 @@
+/**
+ * This script backfills the latest commit on each provided branch.
+ */
+
+import assert from "assert";
+import dotenv from "dotenv";
+import minimist from "minimist";
+
+import { runPipeline } from "./common.mjs";
+
+dotenv.config();
+
+const args = minimist(process.argv.slice(2), {
+    string: ["pipeline"],
+    default: {
+        pipeline: 69,
+    },
+});
+
+const pipeline = args.pipeline;
+assert(pipeline, "Must provide --pipeline");
+
+for (const branch of args._) {
+    const refName = `refs/heads/${branch}`;
+    console.log(`Queueing backfill for ${branch}`);
+
+    await runPipeline(pipeline, {
+        resources: {
+            repositories: {
+                TypeScript: {
+                    refName,
+                },
+            },
+        },
+        templateParameters: {
+            HISTORICAL_RUN: true,
+        },
+    });
+}
