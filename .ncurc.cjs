733ad47af334974f82e1374f63d5d5827e4d289b
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-09-22 16:32:44 -0700

    Switch to pnpm in prep for ts-perf

--- .ncurc.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    target: (dependencyName, [{ semver, version, operator, major, minor, patch, release, build }]) => {
-        if (dependencyName === "@octokit/rest") return "minor";
-        return "latest";
-    },
-};
