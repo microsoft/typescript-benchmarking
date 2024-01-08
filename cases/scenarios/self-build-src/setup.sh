cd4796991518f72ea412193eee5f0c6bdbd73eda
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-01-08 13:11:09 -0800

    Avoid double clone in PRs

--- cases/scenarios/self-build-src/setup.sh
+++ cases/scenarios/self-build-src/setup.sh
@@ -5,10 +5,14 @@ set -exo pipefail
 cd "${0%/*}"
 SCENARIO_NAME=$(basename $PWD)
 SOLUTION_DIR=../../solutions/$SCENARIO_NAME
-rm -rf $SOLUTION_DIR
 
-git clone --filter blob:none https://github.com/microsoft/TypeScript.git $SOLUTION_DIR
+if ! test -d $SOLUTION_DIR; then
+  git clone --filter blob:none https://github.com/microsoft/TypeScript.git $SOLUTION_DIR
+fi
+
 cd $SOLUTION_DIR
+git clean -fdx
+git reset --hard HEAD
 git fetch origin ${TYPESCRIPT_COMMIT}
 git switch --detach ${FETCH_HEAD}
 npm ci
--- cases/scenarios/vscode/setup.sh
+++ cases/scenarios/vscode/setup.sh
@@ -5,9 +5,13 @@ set -exo pipefail
 cd "${0%/*}"
 SCENARIO_NAME=$(basename $PWD)
 SOLUTION_DIR=../../solutions/$SCENARIO_NAME
-rm -rf $SOLUTION_DIR
 
-git clone --filter blob:none https://github.com/microsoft/vscode.git $SOLUTION_DIR
+if ! test -d $SOLUTION_DIR; then
+  git clone --filter blob:none https://github.com/microsoft/vscode.git $SOLUTION_DIR
+fi
+
 cd $SOLUTION_DIR
+git clean -fdx
+git reset --hard HEAD
 git switch --detach f88bce8fe6a6d2ccd27cbd64bb26853cd8779afa
 COREPACK_ENABLE_STRICT=0 corepack yarn@1.22.21 install --ignore-scripts
