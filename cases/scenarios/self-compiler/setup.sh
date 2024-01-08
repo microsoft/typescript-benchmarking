372532fb30bd70add34db59df1464fa9f15c3ed6
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-01-08 13:01:07 -0800

    Fix copy paste error

--- cases/scenarios/self-compiler/setup.sh
+++ cases/scenarios/self-compiler/setup.sh
@@ -7,7 +7,7 @@ SCENARIO_NAME=$(basename $PWD)
 SOLUTION_DIR=../../solutions/$SCENARIO_NAME
 rm -rf $SOLUTION_DIR
 
-git clone --filter blob:none https://github.com/microsoft/vscode.git $SOLUTION_DIR
+git clone --filter blob:none https://github.com/microsoft/TypeScript.git $SOLUTION_DIR
 cd $SOLUTION_DIR
 git fetch origin ${TYPESCRIPT_COMMIT}
 git switch --detach ${FETCH_HEAD}
