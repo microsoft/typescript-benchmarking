f7503de070d8819a124bbdf3a482bdc40f9097a8
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-01-09 13:49:11 -0800

    Add webpack scenario (#8)

--- scripts/src/generateMatrix.ts
+++ scripts/src/generateMatrix.ts
@@ -44,6 +44,7 @@ const scenarioConfig = {
         { name: "self-compiler", agent: "ts-perf2", location: "public" },
         { name: "self-build-src", agent: "ts-perf3", location: "public" },
         { name: "mui-docs", agent: "ts-perf1", location: "public" },
+        { name: "webpack", agent: "ts-perf3", location: "public" },
     ],
     tsserver: [
         { name: "Compiler-UnionsTSServer", agent: "ts-perf1", location: "internal" },
