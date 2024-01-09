086cb2c1e5ae29b5c2935a0f6d3e7276b35a6997
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-01-08 16:38:13 -0800

    Add self link

--- cases/scenarios/webpack/setup.sh
+++ cases/scenarios/webpack/setup.sh
@@ -15,3 +15,8 @@ git clean -fdx
 git reset --hard HEAD
 git switch --detach 228fc69f40c3e9ec6d99a5105fdc85b5bca4ce43
 COREPACK_ENABLE_STRICT=0 corepack yarn@1.22.21 install --ignore-scripts
+
+# https://github.com/webpack/webpack/blob/228fc69f40c3e9ec6d99a5105fdc85b5bca4ce43/.github/workflows/test.yml#L135
+LINK_FOLDER=$PWD/node_modules/.yarn-link
+COREPACK_ENABLE_STRICT=0 corepack yarn@1.22.21 link --link-folder $LINK_FOLDER
+COREPACK_ENABLE_STRICT=0 corepack yarn@1.22.21 link --link-folder $LINK_FOLDER webpack
