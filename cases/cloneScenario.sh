6b132923db4033fb8e377882cc4c4e84ea74b56e
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-01-10 16:38:30 -0800

    Split apart into pinned and unpinned variants

---
+++ cases/cloneScenario.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# https://stackoverflow.com/a/28776166
+(return 0 2>/dev/null) && (echo "This script should be sourced, not executed."; exit 1)
+
+set -exo pipefail
+
+# We're assuming that $PWD is the directory containing scenario.json.
+
+SCENARIO_REPO=$1
+SCENARIO_NAME=${SCENARIO_NAME:-$(basename $PWD)}
+SOLUTION_DIR=${SOLUTION_DIR:-../../solutions/$SCENARIO_NAME}
+
+if ! test -d $SOLUTION_DIR; then
+  git clone --filter blob:none $SCENARIO_REPO $SOLUTION_DIR
+fi
+
+cd $SOLUTION_DIR
+git clean -fdx
+git reset --hard HEAD
+
+if test -z "$SCENARIO_REF"; then
+  SCENARIO_REF=$(git rev-parse HEAD)
+  echo "##vso[task.setvariable variable=SCENARIO_REF]$SCENARIO_REF"
+else
+  git fetch origin $SCENARIO_REF
+  git switch --detach FETCH_HEAD
+fi
