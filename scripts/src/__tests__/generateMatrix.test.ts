dcddd109995fb27b18b539872e15a9f65e7a9060
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-01-16 16:39:53 -0800

    Test full matrix of presets and envs

--- scripts/src/__tests__/generateMatrix.test.ts
+++ scripts/src/__tests__/generateMatrix.test.ts
@@ -8,30 +8,36 @@ import { test } from "vitest";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const tests = [
-    { preset: "full", env: {} },
-    { preset: "full", env: { USE_BASELINE_MACHINE: "true" } },
-    { preset: "regular", env: {} },
-    { preset: "tsc-only", env: {} },
-    { preset: "bun", env: {} },
-    { preset: "vscode", env: {} },
-    { preset: "public", env: {} },
+const presets = [
+    "full",
+    "regular",
+    "tsc-only",
+    "bun",
+    "vscode",
+    "public",
+];
+
+const envs = [
+    {},
+    { USE_BASELINE_MACHINE: "true" },
 ];
 
 // https://github.com/vitest-dev/vitest/issues/4963
 // https://github.com/vitest-dev/vitest/issues/4642
-for (const t of tests) {
-    test.concurrent(`generateMatrix ${util.inspect(t)}`, async ({ expect }) => {
-        const result = await execa(
-            "tsx",
-            [path.join(__dirname, "../generateMatrix.ts"), "--preset", t.preset],
-            {
-                env: t.env,
-                reject: false,
-            },
-        );
-        expect(result.stdout, "stdout").toMatchSnapshot();
-        expect(result.stderr, "stderr").toMatchSnapshot();
-        expect(result.exitCode, "exitCode").toMatchSnapshot();
-    });
+for (const preset of presets) {
+    for (const env of envs) {
+        test.concurrent(`generateMatrix preset=${preset} env=${util.inspect(env)}`, async ({ expect }) => {
+            const result = await execa(
+                "tsx",
+                [path.join(__dirname, "../generateMatrix.ts"), "--preset", preset],
+                {
+                    env,
+                    reject: false,
+                },
+            );
+            expect(result.stdout, "stdout").toMatchSnapshot();
+            expect(result.stderr, "stderr").toMatchSnapshot();
+            expect(result.exitCode, "exitCode").toMatchSnapshot();
+        });
+    }
 }
