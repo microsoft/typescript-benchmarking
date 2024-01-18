d3ba348d1b128510a080fca5087e81ac8eac33fb
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-01-18 10:16:23 -0800

    Overhaul matrix generation, baseline compute cost of presets (#24)

--- cases/scenarios/self-compiler/setup.sh
+++ cases/scenarios/self-compiler/setup.sh
@@ -7,4 +7,8 @@ export SCENARIO_REF=$TYPESCRIPT_COMMIT
 source ../../cloneScenario.sh https://github.com/microsoft/TypeScript.git
 
 npm ci
-npx hereby generate-diagnostics
+if test -f Herebyfile.mjs; then
+  npx hereby generate-diagnostics
+else
+  npx gulp generate-diagnostics
+fi
