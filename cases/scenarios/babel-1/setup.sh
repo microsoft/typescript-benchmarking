b164e0c21d3c8d10fe695f2ab2d86f1a10bf8e0e
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2024-01-20 08:36:49 +0800

    add babel-1

--- cases/scenarios/babel-1/setup.sh
+++ cases/scenarios/babel-1/setup.sh
@@ -3,6 +3,7 @@
 set -exo pipefail
 cd "${0%/*}"
 
+export SCENARIO_REF=eccbd203383487f6957dcf086aa83d773691560b
 source ../../cloneScenario.sh https://github.com/babel/babel.git
 
 COREPACK_ENABLE_STRICT=0 corepack yarn@1.22.21 install --ignore-scripts
