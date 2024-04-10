cebcc78aa815d7796a6eb6daa729731150bc8dde
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-04-09 19:59:46 -0700

    Log differently

--- scripts/src/identityTest.ts
+++ scripts/src/identityTest.ts
@@ -1,12 +1,20 @@
 import { DefaultAzureCredential } from "@azure/identity";
-import { BlobServiceClient } from "@azure/storage-blob";
+import { BlobServiceClient, RestError } from "@azure/storage-blob";
 
-const credential = new DefaultAzureCredential();
-const storageClient = new BlobServiceClient("https://tsperfstorage2.blob.core.windows.net", credential);
+try {
+    const credential = new DefaultAzureCredential();
+    const storageClient = new BlobServiceClient("https://tsperfstorage2.blob.core.windows.net", credential);
 
-const blob = await storageClient
-    .getContainerClient("benchmark")
-    .getBlobClient("main/latest.linux.benchmark")
-    .downloadToBuffer();
+    const blob = await storageClient
+        .getContainerClient("benchmark")
+        .getBlobClient("main/latest.linux.benchmark")
+        .downloadToBuffer();
 
-console.log(blob.toString("utf-8"));
+    console.log(blob.toString("utf-8"));
+}
+catch (e) {
+    if (e instanceof RestError) {
+        console.error(e.name, e.statusCode, e.details);
+    }
+    process.exit(1);
+}
