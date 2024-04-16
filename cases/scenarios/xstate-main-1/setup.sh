266b94a4ec65530b29b77605277005a21d00f08c
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-04-16 15:58:35 -0700

    Bump to 5.11

--- cases/scenarios/xstate-main-1/setup.sh
+++ cases/scenarios/xstate-main-1/setup.sh
@@ -5,6 +5,6 @@ cd "${0%/*}"
 
 source ../../common.sh
 
-clone_scenario https://github.com/statelyai/xstate.git 424ce971778236018f5a6447505909307880a687
+clone_scenario https://github.com/statelyai/xstate.git 9118720b2d81cd3cd6b8e4ea8da75d576c47fa8d
 
 run_sandboxed sh -c 'yarn install'
