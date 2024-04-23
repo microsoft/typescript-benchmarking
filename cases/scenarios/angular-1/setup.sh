2f81cb698899ff3ab3efc7904f7d2e02af8f2ec3
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-04-22 21:55:19 -0700

    Actually, try packages

--- cases/scenarios/angular-1/setup.sh
+++ cases/scenarios/angular-1/setup.sh
@@ -5,11 +5,6 @@ cd "${0%/*}"
 
 source ../../common.sh
 
-clone_scenario https://github.com/angular/angular.git 9894278e712a50079af87898a63e1d19a462d015
+clone_scenario https://github.com/angular/angular.git cc57d4c4998b4e38f940afdf358af37185028072
 
-# Shockingly, angular compiles just fine without installing any dependencies.
-run_sandboxed sh -c '
-    npx json5 tsconfig-tslint.json > tsconfig-tslint.json.new &&
-    mv tsconfig-tslint.json.new tsconfig-tslint.json &&
-    npx json -I -f tsconfig-tslint.json -e "this.exclude = [\"**/test_cases/**\"]"
-'
+run_sandboxed sh -c 'yarn install --ignore-scripts'
