008ba6866293e03d3d5915ed354135d21e6cfc0b
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-04-22 20:35:36 -0700

    Don't even install for angular, wow

--- cases/scenarios/angular-1/setup.sh
+++ cases/scenarios/angular-1/setup.sh
@@ -7,10 +7,9 @@ source ../../common.sh
 
 clone_scenario https://github.com/angular/angular.git 9894278e712a50079af87898a63e1d19a462d015
 
+# Shockingly, angular compiles just fine without installing any dependencies.
 run_sandboxed sh -c '
-    export PUPPETEER_SKIP_DOWNLOAD=true &&
     npx json5 tsconfig-tslint.json > tsconfig-tslint.json.new &&
     mv tsconfig-tslint.json.new tsconfig-tslint.json &&
-    npx json -I -f tsconfig-tslint.json -e "this.exclude = [\"**/test_cases/**\"]" &&
-    yarn install
+    npx json -I -f tsconfig-tslint.json -e "this.exclude = [\"**/test_cases/**\"]"
 '
