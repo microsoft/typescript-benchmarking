280aaf4348c3abeb90a4b7f36e54e401895fb7b9
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-04-22 19:54:44 -0700

    Drop unused var

--- cases/scenarios/vscode-1/setup.sh
+++ cases/scenarios/vscode-1/setup.sh
@@ -5,8 +5,6 @@ cd "${0%/*}"
 
 source ../../common.sh
 
-RUN_SANDBOXED=$(realpath ../../runSandboxed.sh)
-
 clone_scenario https://github.com/microsoft/vscode.git f88bce8fe6a6d2ccd27cbd64bb26853cd8779afa
 
 run_sandboxed sh -c 'yarn install --ignore-scripts'
