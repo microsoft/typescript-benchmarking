55f68d799b40e279c432e756e2b879f471ac89c1
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-09-16 10:15:42 -0700

    Set PUPPETEER_SKIP_CHROMIUM_DOWNLOAD

--- cases/scenarios/xstate-main/setup.sh
+++ cases/scenarios/xstate-main/setup.sh
@@ -7,4 +7,4 @@ source ../../common.sh
 
 clone_scenario https://github.com/statelyai/xstate.git
 
-run_sandboxed sh -c 'npx $(node -e "console.log(JSON.parse(fs.readFileSync(\"package.json\", \"utf8\")).packageManager)") install'
+run_sandboxed sh -c 'PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true npx $(node -e "console.log(JSON.parse(fs.readFileSync(\"package.json\", \"utf8\")).packageManager)") install'
